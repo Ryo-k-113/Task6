35ecefe15f08b1e3b5390b88940a79ea909cd432
Ryo-k-113 <r.kameda.113@gmail.com>
2025-06-15 11:56:35 +0900

    タスク6-1をアップ

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Header } from './components/Header'
+import { Main } from './components/Main.JSX'
+
+export const App = () => {
+
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
