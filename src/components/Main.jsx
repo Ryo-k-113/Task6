986a43216ad3a46b37abb2c907a03f5cf0317d9a
Ryo-k-113 <r.kameda.113@gmail.com>
2025-06-15 16:50:43 +0900

    リファクタリングを実施

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,22 +8,20 @@ export const Main = () => {
         {posts.map(post =>  {
           return(
             <li key={post.id} className="postList border-1 border-gray-300 mt-10 pl-4 pr-12 py-4">
-              <div>
-                <a href={post.thumbnailUrl}>
-                 <div className="flex justify-between">
-                    <p className="postDate text-gray-500 text-xs">{new Date(post.createdAt).toLocaleDateString()}</p>
-                    <div className="flex gap-x-2 items-center">
-                      {post.categories.map((category) => {
-                        return(
-                         <p className="category text-sm text-fuchsia-600 border-1 rounded-sm p-1">{category}</p>
-                        );
-                      })}
-                    </div>
+              <a href={post.thumbnailUrl}>
+                <div className="flex justify-between">
+                  <p className="postDate text-gray-500 text-xs">{new Date(post.createdAt).toLocaleDateString()}</p>
+                  <div className="flex gap-x-2 items-center">
+                    {post.categories.map((category) => {
+                      return(
+                        <p className="category text-sm text-fuchsia-600 border-1 rounded-sm p-1">{category}</p>
+                      )
+                    })}
                   </div>
-                  <div className="postTitle text-2xl font-medium mt-3">APIで取得した{post.title}</div>
-                  <p className="postContent mt-3 line-clamp-2" dangerouslySetInnerHTML={{ __html:post.content}}></p>
-                </a>
-              </div>
+                </div>
+                <div className="postTitle text-2xl font-medium mt-3">APIで取得した{post.title}</div>
+                <p className="postContent mt-3 line-clamp-2" dangerouslySetInnerHTML={{ __html:post.content}}></p>
+              </a>
             </li>
           );
         })}
